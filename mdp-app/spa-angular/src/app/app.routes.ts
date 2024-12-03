f90fcda6823b51c327f4694359552007df1a1ae4
puspita <puspitasari22232@gmail.com>
2024-12-03 09:22:03 +0700

    push

--- mdp-app/spa-angular/src/app/app.routes.ts
+++ mdp-app/spa-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FakultasComponent } from './components/fakultas/fakultas.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'fakultas', component: FakultasComponent }
+];
