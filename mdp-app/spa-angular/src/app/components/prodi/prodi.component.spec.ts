f5dda64b2ab2c2f807532f3e54682a9f894f866c
puspita <puspitasari22232@gmail.com>
2024-12-06 09:08:53 +0700

    push

--- mdp-app/spa-angular/src/app/components/prodi/prodi.component.spec.ts
+++ mdp-app/spa-angular/src/app/components/prodi/prodi.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-import { ProdiComponent } from './prodi.component';
-
-describe('ProdiComponent', () => {
-  let component: ProdiComponent;
-  let fixture: ComponentFixture<ProdiComponent>;
-
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProdiComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ProdiComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports: [AppComponent],
+    }).compileComponents();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'spa-angular' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('spa-angular');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spa-angular');
   });
 });
